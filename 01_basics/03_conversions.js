0f8fb5137b612922fa1eb28418a6745e44de9c86
shrivanshhh... <shrivanshsinghal2020@gmail.com>
2025-02-10 19:45:50 +0000

    commit this file

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -15,3 +15,26 @@ let booleanloggedin = Boolean(loggedin)
 //console.log(booleanloggedin);
 
 //1 => true  0 => false
+
+//OPERATIONS//
+
+let digit =3
+let negvalue = -digit
+
+//console.log(negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%3);
+
+let str1= "hello"
+let str2=  " shrivansh"
+
+let str3= str1+str2
+console.log(str3);
+
+
+
+
