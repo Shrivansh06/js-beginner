a1324bee6b07e90fc743d9140e411c401cee32a9
shrivanshhh... <shrivanshsinghal2020@gmail.com>
2025-02-10 19:32:58 +0000

    commit these changes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+let age = 18
+let name="aadi"
+let isloggedin = true
+
+//number = 2 to power 53
+//string = ""
+//bigint = for big number values
+// boolean = true/ false
+//null
+//undefined
+
+//object
+
+
+//console.log(typeof "data type");
---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,17 @@
+let score = "33"
+let name = null
+//console.log(typeof score);
+//console.log(typeof name);
+
+let value = Number(score)
+
+//console.log(typeof value);
+//console.log(value);
+
+let loggedin = true
+
+let booleanloggedin = Boolean(loggedin)
+
+//console.log(booleanloggedin);
+
+//1 => true  0 => false
